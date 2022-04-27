68a9de2b02ecfc553ec74418c4bd358a6b70c529
Gustavo Gaiotti <gustavogaiotti2014@gmail.com>
2022-04-26 23:39:04 -0300

    execicio desafio 2

---
+++ Desafio_Fase#2.py
@@ -0,0 +1,13 @@
+adobe = ['Adobe','e-mail','dica','senha','nome']
+apollo = ['Apollo','e-mail','nome','telefone']
+canva = ['Canva','e-mail','nome','senha']
+pdl = ['PDL','e-mail','nome','telefone']
+hurb = ['Hurb','e-mail','senha','telefone']
+lista_primaria = [adobe, apollo, canva, pdl, hurb]
+lista_senha = []
+lista_senha = []
+for i in range(len(lista_primaria)):
+    if 'senha' in lista_primaria[i]:
+        lista_senha.append(lista_primaria[i][0])
+    elif
+print(str(lista_senha))
